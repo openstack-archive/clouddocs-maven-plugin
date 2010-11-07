803098b5076636d4b43a2dbbfb4e52643c4d2f0c
Jorge L. Williams <jorge.williams@rackspace.com>
2010-11-07 13:59:24 -0600

    We now correctly adjust graphics paths.

--- src/main/java/com/rackspace/cloud/api/docs/PDFMojo.java
+++ src/main/java/com/rackspace/cloud/api/docs/PDFMojo.java
@@ -2,11 +2,23 @@ package com.rackspace.cloud.api.docs;
 
 import java.io.File;
 
+import javax.xml.transform.Transformer;
+
 import org.apache.maven.plugin.MojoExecutionException;
 import com.agilejava.docbkx.maven.AbstractPdfMojo;
 import com.rackspace.cloud.api.docs.FileUtils;
 
 public abstract class PDFMojo extends AbstractPdfMojo {
+    private File imageDirectory;
+
+    protected void setImageDirectory (File imageDirectory) {
+        this.imageDirectory = imageDirectory;
+    }
+
+    protected File getImageDirectory() {
+        return this.imageDirectory;
+    }
+
     /*
        Setup..
     */
@@ -24,5 +36,19 @@ public abstract class PDFMojo extends AbstractPdfMojo {
         // Extract all images into the image directory.
         //
         FileUtils.extractJaredDirectory("images",PDFMojo.class,imageParentDirectory);
+        setImageDirectory (new File (imageParentDirectory, "images"));
+    }
+
+    public void adjustTransformer(Transformer transformer, String sourceFilename, File targetFile) {
+        super.adjustTransformer(transformer, sourceFilename, targetFile);
+
+        //
+        //  Setup graphics paths
+        //
+        File imageDirectory = getImageDirectory();
+        File calloutDirectory = new File (imageDirectory, "callouts");
+
+        transformer.setParameter ("admon.graphics.path", imageDirectory.getAbsolutePath()+File.separator);
+        transformer.setParameter ("callout.graphics.path", calloutDirectory.getAbsolutePath()+File.separator);
     }
 }
