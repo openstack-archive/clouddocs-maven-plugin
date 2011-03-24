ad9e0a7606964dbaf0f6b53ee53eec4e64e422d9
dcramer <david@thingbag.net>
2011-03-24 10:25:57 -0500

    Modify webhelp to use a customization layer for the xslt.

--- src/main/java/com/rackspace/cloud/api/docs/WebHelpMojo.java
+++ src/main/java/com/rackspace/cloud/api/docs/WebHelpMojo.java
@@ -1,6 +1,51 @@
 package com.rackspace.cloud.api.docs;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import java.net.URL;
+
+import java.security.CodeSource;
+import java.util.*;
+import java.util.zip.ZipInputStream;
+
+import javax.xml.transform.Transformer;
+
+import org.apache.maven.plugin.MojoExecutionException;
+
+import com.nexwave.nquindexer.SaxHTMLIndex;
+import com.nexwave.nquindexer.WriteJSFiles;
+
+import com.nexwave.nsidita.DirList;
+import com.nexwave.nsidita.DocFileInfo;
 import com.agilejava.docbkx.maven.AbstractWebhelpMojo;
 
+import com.agilejava.docbkx.maven.TransformerBuilder;
+import javax.xml.transform.URIResolver;
+import com.rackspace.cloud.api.docs.DocBookResolver;
+
 public abstract class WebHelpMojo extends AbstractWebhelpMojo {
+
+    protected TransformerBuilder createTransformerBuilder(URIResolver resolver) {
+        return super.createTransformerBuilder (new DocBookResolver (resolver, getType()));
+    }
+
+      protected String getNonDefaultStylesheetLocation() {
+      return "cloud/webhelp/docbook.xsl";
+    }
+
 }
+
+
+/*
+
+  private String brandingFoobar;
+
+  public void postProcess() throws MojoExecutionException {
+    super.postProcess();
+    getLog().info("FOOBAR!!! " + brandingFoobar + " BARFOO!!");
+  }
+
+
+*/
---
+++ src/main/resources/cloud/webhelp/docbook.xsl
@@ -0,0 +1,9 @@
+<?xml version="1.0"?>
+<xsl:stylesheet exclude-result-prefixes="d"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:d="http://docbook.org/ns/docbook"
+                version="1.0">
+
+  <xsl:import href="urn:docbkx:stylesheet-orig/xsl/webhelp.xsl" />
+
+</xsl:stylesheet>
