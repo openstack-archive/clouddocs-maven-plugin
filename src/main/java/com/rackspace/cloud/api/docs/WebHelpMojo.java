b44d2606db1105e975917588c3393b1391207f06
dcramer <david@thingbag.net>
2011-03-27 15:03:02 -0500

    Support webhelp branded as openstack or rackspace.

--- src/main/java/com/rackspace/cloud/api/docs/WebHelpMojo.java
+++ src/main/java/com/rackspace/cloud/api/docs/WebHelpMojo.java
@@ -13,17 +13,14 @@ import java.util.zip.ZipInputStream;
 import javax.xml.transform.Transformer;
 
 import org.apache.maven.plugin.MojoExecutionException;
-
-import com.nexwave.nquindexer.SaxHTMLIndex;
-import com.nexwave.nquindexer.WriteJSFiles;
-
-import com.nexwave.nsidita.DirList;
-import com.nexwave.nsidita.DocFileInfo;
 import com.agilejava.docbkx.maven.AbstractWebhelpMojo;
 
 import com.agilejava.docbkx.maven.TransformerBuilder;
 import javax.xml.transform.URIResolver;
 import com.rackspace.cloud.api.docs.DocBookResolver;
+
+import com.agilejava.docbkx.maven.Parameter;
+import com.agilejava.docbkx.maven.FileUtils;
 
 public abstract class WebHelpMojo extends AbstractWebhelpMojo {
 
@@ -31,21 +28,40 @@ public abstract class WebHelpMojo extends AbstractWebhelpMojo {
         return super.createTransformerBuilder (new DocBookResolver (resolver, getType()));
     }
 
-      protected String getNonDefaultStylesheetLocation() {
+    //Note for this to work, you need to have the customization layer in place.
+    protected String getNonDefaultStylesheetLocation() {
       return "cloud/webhelp/docbook.xsl";
     }
+    
+    public void postProcessResult(File result) throws MojoExecutionException {
+	super.postProcessResult(result);
+	
+	copyTemplate(result);	
+    }
+
+    protected void copyTemplate(File result) throws MojoExecutionException {
+
+	final File targetDirectory = result.getParentFile();
+
+	com.rackspace.cloud.api.docs.FileUtils.extractJaredDirectory("content",WebHelpMojo.class,targetDirectory);
+	com.rackspace.cloud.api.docs.FileUtils.extractJaredDirectory("common",WebHelpMojo.class,targetDirectory);
+
+	if (getCustomizationParameters() != null) {
+	    getLog().info("Listing customization parameters");
+	    final Iterator iterator = getCustomizationParameters()
+		.iterator();
+	    while (iterator.hasNext()) {
+		com.agilejava.docbkx.maven.Parameter param = (com.agilejava.docbkx.maven.Parameter) iterator.next();
+		if (param.getName().equals("branding")) 
+		    {
+			getLog().info("Copying favicon.ico");
+			com.agilejava.docbkx.maven.FileUtils.copyFile(new File(targetDirectory,"common/images/favicon-" + param.getValue() + ".ico"), new File(targetDirectory,"favicon.ico"));
+
+		    }		
+	    }
+	}
+
+    }
+
 
 }
-
-
-/*
-
-  private String brandingFoobar;
-
-  public void postProcess() throws MojoExecutionException {
-    super.postProcess();
-    getLog().info("FOOBAR!!! " + brandingFoobar + " BARFOO!!");
-  }
-
-
-*/
