31400bf276ab060ce014dc046747aa15c4c84921
dcramer <david@thingbag.net>
2011-03-31 09:56:23 -0500

    Initial support for eclipse and epub. Epub currently fails on the zip step.

---
+++ src/main/java/com/rackspace/cloud/api/docs/EclipseMojo.java
@@ -0,0 +1,27 @@
+package com.rackspace.cloud.api.docs;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import java.net.URL;
+
+import java.security.CodeSource;
+import java.util.*;
+import java.util.zip.ZipInputStream;
+
+import javax.xml.transform.Transformer;
+
+import org.apache.maven.plugin.MojoExecutionException;
+
+import com.agilejava.docbkx.maven.TransformerBuilder;
+import javax.xml.transform.URIResolver;
+import com.rackspace.cloud.api.docs.DocBookResolver;
+
+import com.agilejava.docbkx.maven.Parameter;
+import com.agilejava.docbkx.maven.FileUtils;
+import com.agilejava.docbkx.maven.AbstractEclipseMojo;
+
+public abstract class EclipseMojo extends AbstractEclipseMojo {
+
+}
