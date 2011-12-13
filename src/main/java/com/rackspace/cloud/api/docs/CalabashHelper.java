c9682c0181454b944891ee4607a7cd485073e4a3
Seth <sethbrayman@gmail.com>
2011-12-13 16:54:07 -0600

    Updating Calabash helper for legacy source. Updating pom for newer version of papi. Updating PDFMojo to use calabash. TODOs for refactoring.

--- src/main/java/com/rackspace/cloud/api/docs/CalabashHelper.java
+++ src/main/java/com/rackspace/cloud/api/docs/CalabashHelper.java
@@ -6,22 +6,22 @@ import com.rackspace.papi.components.translation.xproc.calabash.CalabashPipeline
 import org.xml.sax.InputSource;
 
 import javax.xml.transform.Source;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CalabashHelper {
-    public static void run(final String pipelineUri, final InputStream inputStream) throws FileNotFoundException {
-        Pipeline pipe = new CalabashPipelineBuilder(false).build(pipelineUri);
+    public static Source run(final String pipelineURI, final InputSource inputSource) throws FileNotFoundException {
+        Pipeline pipeline = new CalabashPipelineBuilder(false, true).build(pipelineURI);
 
         List<PipelineInput> inputs = new ArrayList<PipelineInput>() {{
-            add(PipelineInput.port("source", new InputSource(inputStream)));
+            add(PipelineInput.port("source", inputSource));
         }};
 
-        pipe.run(inputs);
+        pipeline.run(inputs);
 
-        List<Source> nodes = pipe.getResultPort("result"); // result of xinclude;
+        List<Source> sources = pipeline.getResultPort("result"); // result of xinclude;
+
+        return sources.get(0);
     }
 }
