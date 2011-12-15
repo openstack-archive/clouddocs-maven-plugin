8bdaec52b220c19f603582b9e087b062e1ba344f
Seth <sethbrayman@gmail.com>
2011-12-15 16:37:19 -0600

    Refactoring and  test setup.

--- src/main/java/com/rackspace/cloud/api/docs/CalabashHelper.java
+++ src/main/java/com/rackspace/cloud/api/docs/CalabashHelper.java
@@ -3,25 +3,44 @@ package com.rackspace.cloud.api.docs;
 import com.rackspace.papi.components.translation.xproc.Pipeline;
 import com.rackspace.papi.components.translation.xproc.PipelineInput;
 import com.rackspace.papi.components.translation.xproc.calabash.CalabashPipelineBuilder;
+import org.apache.maven.plugin.MojoExecutionException;
 import org.xml.sax.InputSource;
 
 import javax.xml.transform.Source;
+import javax.xml.transform.sax.SAXSource;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CalabashHelper {
-    public static Source run(final String pipelineURI, final InputSource inputSource) throws FileNotFoundException {
+    private static Source run(final String pipelineURI, final InputSource inputSource) throws FileNotFoundException {
         Pipeline pipeline = new CalabashPipelineBuilder(false, true).build(pipelineURI);
 
-        List<PipelineInput> inputs = new ArrayList<PipelineInput>() {{
+        List<PipelineInput> pipelineInputs = new ArrayList<PipelineInput>() {{
             add(PipelineInput.port("source", inputSource));
         }};
 
-        pipeline.run(inputs);
+        pipeline.run(pipelineInputs);
 
         List<Source> sources = pipeline.getResultPort("result"); // result of xinclude;
 
         return sources.get(0);
+    }
+
+    public static Source createSource(Source source)
+            throws MojoExecutionException {
+
+        String pipelineURI = "/Users/sbrayman/Documents/workspace/rc-maven-cloud-docs/src/main/resources/test.xpl"; //TODO: Pull this out to someplace better?
+
+        try {
+            if (!(source instanceof SAXSource)) {
+                throw new MojoExecutionException("Expecting a SAXSource");
+            }
+            SAXSource saxSource = (SAXSource) source;
+
+            return run(pipelineURI, saxSource.getInputSource());
+        } catch (FileNotFoundException e) {
+            throw new MojoExecutionException("Failed to find source.", e);
+        }
     }
 }
