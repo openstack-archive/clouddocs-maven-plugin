2dd4c256373e1113b9a77857ec6b65359bedb1eb
Seth <sethbrayman@gmail.com>
2011-12-21 16:52:20 -0600

    Made change to use relative path for pipeline file.  Changed test. Updated POM.

--- src/test/java/com/rackspace/cloud/api/docs/CalabashHelperTest.java
+++ src/test/java/com/rackspace/cloud/api/docs/CalabashHelperTest.java
@@ -5,10 +5,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
-import org.xml.sax.InputSource;
 
 import javax.xml.transform.Source;
-import javax.xml.transform.sax.SAXSource;
 
 import static org.mockito.Mockito.mock;
 
@@ -21,20 +19,16 @@ import static org.mockito.Mockito.mock;
 public class CalabashHelperTest {
     public static class whenUsingCalabashHelper {
 
-        String pipelineURI = null;
-        InputSource inputSource = null;
         Source source = null;
 
         @Before
         public void setUp() throws Exception {
-            pipelineURI = "/Users/sbrayman/Documents/workspace/rc-maven-cloud-docs/src/main/resources/test.xpl";
-            inputSource = mock(InputSource.class);
             source = mock(Source.class);
         }
 
         @Test (expected=MojoExecutionException.class)
         public void shouldThrowSAXSourceError() throws Exception {
-            CalabashHelper.createSource(source);
+            CalabashHelper.createSource(source, null);
         }
     }
 }
