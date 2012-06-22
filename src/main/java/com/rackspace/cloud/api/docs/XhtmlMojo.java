be9289ff794da384c7e30734717bff74e7a733fd
dcramer <david.cramer@rackspace.com>
2012-06-22 12:07:25 -0500

    Pass in all the params to xproc.

--- src/main/java/com/rackspace/cloud/api/docs/XhtmlMojo.java
+++ src/main/java/com/rackspace/cloud/api/docs/XhtmlMojo.java
@@ -42,6 +42,153 @@ public abstract class XhtmlMojo extends AbstractHtmlMojo {
      *     default-value=""
      */
     private String security;
+    
+    
+    
+        /**
+     * Controls whether output is colorized based on revisionflag attributes.
+     *
+     * @parameter expression="${generate-webhelp.show.changebars}"
+     */
+    private String showChangebars;
+    
+     /**
+     * Display built for OpenStack logo?
+     *
+     * @parameter expression="${generate-webhelp.builtForOpenStack}" default-value="0"
+     */
+    private String builtForOpenStack;
+
+    /**
+     * Controls whether output is colorized based on revisionflag attributes.
+     *
+     * @parameter expression="${generate-webhelp.meta.robots}" 
+     */
+    private String metaRobots;
+
+    /**
+     * Controls whether the version string is used as part of the Disqus identifier.
+     *
+     * @parameter expression="${generate-webhelp.use.version.for.disqus}" default-value="0"
+     */
+    private String useVersionForDisqus;
+
+    /**
+     * Controls whether the disqus identifier is used.
+     *
+     * @parameter expression="${generate-webhelp.use.disqus.id}" default-value="1"
+     */
+    private String useDisqusId;
+
+    /**
+     * Controls the branding of the output.
+     *
+     * @parameter expression="${generate-webhelp.branding}" default-value="rackspace"
+     */
+    private String branding;
+
+    /**
+     * Controls whether Disqus comments appear at the bottom of each page.
+     *
+     * @parameter expression="${generate-webhelp.enable.disqus}" default-value="0"
+     */
+    private String enableDisqus;
+
+    /**
+     * A parameter used by the Disqus comments.
+     *
+     * @parameter expression="${generate-webhelp.disqus.shortname}" default-value=""
+     */
+    private String disqusShortname;
+
+    /**
+     * A parameter used to control whether to include Google Analytics goo.
+     *
+     * @parameter expression="${generate-webhelp.enable.google.analytics}" default-value=""
+     */
+    private String enableGoogleAnalytics;
+
+    /**
+     * A parameter used to control whether to include Google Analytics goo.
+     *
+     * @parameter expression="${generate-webhelp.google.analytics.id}" default-value=""
+     */
+    private String googleAnalyticsId;
+
+    /**
+     * A parameter used to specify the path to the pdf for download in webhelp.
+     *
+     * @parameter expression="${generate-webhelp.pdf.url}" default-value=""
+     */
+    private String pdfUrl;
+
+    /**
+     * @parameter 
+     *     expression="${generate-webhelp.canonicalUrlBase}"
+     *     default-value=""
+     */
+    private String canonicalUrlBase;
+ 
+    /**
+     * 
+     *
+     * @parameter expression="${basedir}"
+     */
+    private File baseDir;
+
+    /**
+     * A parameter used to specify the presence of extensions metadata.
+     *
+     * @parameter 
+     *     expression="${generate-webhelp.includes}" 
+     *     default-value=""
+     */
+    private String transformDir;   
+    
+    /**
+     * A parameter used to configure how many elements to trim from the URI in the documentation for a wadl method.
+     *
+     * @parameter expression="${generate-webhelp.trim.wadl.uri.count}" default-value=""
+     */
+    private String trimWadlUriCount;
+
+    /**
+     * Controls how the path to the wadl is calculated. If 0 or not set, then
+     * The xslts look for the normalized wadl in /generated-resources/xml/xslt/.
+     * Otherwise, in /generated-resources/xml/xslt/path/to/docbook-src, e.g.
+     * /generated-resources/xml/xslt/src/docbkx/foo.wadl
+     *
+     * @parameter expression="${generate-webhelp.compute.wadl.path.from.docbook.path}" default-value="0"
+     */
+    private String computeWadlPathFromDocbookPath;
+
+     /**
+      * Sets the email for TildeHash (internal) comments. Note that this
+      * doesn't affect Disqus comments.
+      *
+      * @parameter expression="${generate-webhelp.feedback.email}" default-value=""
+      */
+    private String feedbackEmail;
+
+     /**
+      * Controls whether or not the social icons are displayed.
+      *
+      * @parameter expression="${generate-webhelp.social.icons}" default-value="0"
+      */
+    private String socialIcons;
+    /**
+     * A parameter used to specify the path to the lega notice in webhelp.
+     *
+     * @parameter expression="${generate-webhelp.legal.notice.url}" default-value="index.html"
+     */
+    private String legalNoticeUrl;
+    
+    
+    
+    
+    
+    
+    
 
     protected TransformerBuilder createTransformerBuilder(URIResolver resolver) {
         return super.createTransformerBuilder (new DocBookResolver (resolver, getType()));
@@ -132,10 +279,63 @@ public abstract class XhtmlMojo extends AbstractHtmlMojo {
 
         Map map=new HashMap<String, String>();
         
-        map.put("security", security);
         map.put("failOnValidationError", failOnValidationError);
+        map.put("transform.dir", transformDir);        
+             
+    if(feedbackEmail != null){
+      map.put("feedback.email", feedbackEmail);
+    }
+    if(useDisqusId != null){
+	   map.put("use.disqus.id", useDisqusId);
+    }
+    if (useVersionForDisqus != null) {
+       map.put("use.version.for.disqus", useVersionForDisqus);
+    }
         map.put("project.build.directory", projectBuildDirectory);
+        map.put("branding", branding);
+        map.put("builtForOpenStack", builtForOpenStack);
+        map.put("enable.disqus", enableDisqus);
+        if (disqusShortname != null) {
+            map.put("disqus.shortname", disqusShortname);
+        }
+        if (enableGoogleAnalytics != null) {
+            map.put("enable.google.analytics", enableGoogleAnalytics);
+        }
+        if (googleAnalyticsId != null) {
+            map.put("google.analytics.id", googleAnalyticsId);
+        }
+        if (pdfUrl != null) {
+            map.put("pdf.url", pdfUrl);
+        }
+        if (legalNoticeUrl != null) {
+            map.put("legal.notice.url", legalNoticeUrl);
+        }
 
+    if(canonicalUrlBase != null){
+	map.put("canonical.url.base",canonicalUrlBase);
+    }
+
+    if(security != null){
+	map.put("security",security);
+    }
+   if(showChangebars != null){
+	map.put("show.changebars",showChangebars);
+    }
+   if(metaRobots != null){
+	map.put("meta.robots",metaRobots);
+    }
+   if(trimWadlUriCount != null){
+	map.put("trim.wadl.uri.count",trimWadlUriCount);
+    }
+
+	map.put("social.icons",socialIcons);
+
+//   sourceDocBook = new File(sourceFilename);
+//   sourceDirectory = sourceDocBook.getParentFile();
+//   map.put("docbook.infile",sourceDocBook.getAbsolutePath());
+//   map.put("source.directory",sourceDirectory);
+           
+        
         int lastSlash=inputFilename.lastIndexOf("/");
 
         //This is the case if the path includes a relative path
